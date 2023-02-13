8af525a83bf0cfda09aea64a9a1419b1703f7a40
The98-git <115868350+The98-git@users.noreply.github.com>
2023-02-13 06:05:40 +0400

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home"
+import Offers from "./pages/Offers"
+import Profile from "./pages/Profile"
+import SignIn from "./pages/SignIn"
+import Signup from "./pages/Signup"
+import ForgotPassword from "./pages/ForgotPassword"
 function App() {
   return (
     <>
-      <h1 className='text-2x1 bg-red-500'>Hello</h1>
-    </>
+	<Router>
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/profile" element={<Profile />} />
+			<Route path="/sign-in" element={<SignIn />} />
+			<Route path="/sign-up" element={<Signup />} />
+			<Route path="/forgot-password" element={<ForgotPassword />} />
+			<Route path="/offers" element={<Offers />} />
+		</Routes>
+	</Router>
+   </>
   );
 }
 
