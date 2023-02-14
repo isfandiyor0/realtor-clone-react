51376b54e921b92adf9a858b461c7597f7a7a24b
The98-git <115868350+The98-git@users.noreply.github.com>
2023-02-14 06:58:44 +0400

    fixed some mistake an Sign in Sign Up Resset Pass page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -5,8 +5,9 @@ import OAuth from '../components/OAuth';
 export default function SignIn() {
 	const [showPassword, setShowPassword] = useState(false);
 	const [formData, setFormData] = useState({
-		emeil: "",
+		email: "",
 		password: "",
+		name: "",
 	});
 	const {email, password} = formData;
 	function onChange(e) {
@@ -17,7 +18,7 @@ export default function SignIn() {
 	}
   return (
 	<section>
-		<h1 className='text-3xl text-center mt-6 font-bold'>Sign In</h1>
+		<h1 className='text-3xl text-center mt-6 font-bold'>Sign Up</h1>
 		<div 
 			className='flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto'>
 			<div className='md:w-[67%] lg:w-[50%] mb-12 md:mb-6'>
@@ -29,6 +30,7 @@ export default function SignIn() {
 					<input 
 						className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-out' 
 						type="text" 
+						// value={name}
 						onChange={onChange} 
 						id="name" 
 						placeholder='Full Name'/>
@@ -81,7 +83,7 @@ export default function SignIn() {
 						<p>Have an account?
 							<Link 
 								className='text-red-600 hover:text-red-700 transition duration-200 easy-in-out ml-2'
-								to="/sign-in">Sign In
+								to="/sign-in">Sign Up
 							</Link>
 						</p>
 					</div>
@@ -90,7 +92,7 @@ export default function SignIn() {
 					type='submit'
 					className='mt-6 bg-blue-600 w-full text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800'
 					>
-					Sign In
+					Sign Up
 				</button>
 				<div 
 					className='flex items-center my-4 before:border-t  before:flex-1  before:border-gray-300 after:border-t after:flex-1  after:border-gray-300'>
